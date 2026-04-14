52178485c91b4b0b597212d1b50502dcccf8e584
LazzyCC <lorenzo.c.casoni@gmail.com>
2026-04-14 14:03:59 +0200

    fix: resolve CORS issues and debounce Overpass API calls

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins=["*"], 
-    allow_methods=["*"], 
-    allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/hackathon_db', pool_pre_ping=True)
